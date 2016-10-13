4635aeb49199f5b5d9b0736f6214b91bbe1eb082
Goga Chinchaladze <goga.chinchaladze@gmail.com>
2016-10-13 12:39:32 +0400

    Just Indentation things

--- lineit.js
+++ lineit.js
@@ -1,6 +1,6 @@
-var lineIt = (function(){
+var lineIt = (function () {
     //Line Color, Line height, Line z-position, Transition duration, Transition timing function, Hover/always
-    function createLine(color, height, animationTime, animationTimingFunction){
+    function createLine(color, height, animationTime, animationTimingFunction) {
         var lineDiv = document.createElement("div");
         lineDiv.className = "line";
         lineDiv.style.backgroundColor = color;
@@ -9,11 +9,11 @@ var lineIt = (function(){
         lineDiv.style.webkitTransitionDuration = animationTime;
         lineDiv.style.transitionTimingFunction = animationTimingFunction;
         lineDiv.style.webkitTransitionTimingFunction = animationTimingFunction;
-        
+
         return lineDiv;
     }
 
-    function drawLine(el){
+    function drawLine(el) {
         var sentence = el.innerHTML,
             words = sentence.split(" "),
             lineColor = el.getAttribute('data-line-color'),
@@ -26,12 +26,12 @@ var lineIt = (function(){
         el.className += " line-it"; //Adding basic class
         el.className += (linePosition) ? " " + linePosition : ""; //Adding position class
         el.className += (hoverOnly) ? " hover-only" : ""; //adding hover/not hover class
-        
-        if(linePosition == "through" || linePosition === undefined){ //converting string to spans
+
+        if (linePosition == "through" || linePosition === undefined) { //converting string to spans
             var htmlString = "";
-            for(var i=0,length=words.length; i<length; i++){
+            for (var i = 0, length = words.length; i < length; i++) {
                 var chars = words[i].split('');
-                for(var j=0, charslength=chars.length; j<charslength; j++){
+                for (var j = 0, charslength = chars.length; j < charslength; j++) {
                     var charInSpan = '<span>' + chars[j] + '</span>'
                     htmlString += charInSpan;
                 }
@@ -39,23 +39,23 @@ var lineIt = (function(){
             }
             el.innerHTML = htmlString;
         }
-        var line = createLine(lineColor,lineHeight, animationTime, animationTimingFunction);
+        var line = createLine(lineColor, lineHeight, animationTime, animationTimingFunction);
         el.appendChild(line);
     }
 
 
-    function init(){
+    function init() {
         var lineItEls = document.querySelectorAll('[data-line-it]');
-        for(var i = 0, length = lineItEls.length; i<length; i++){
+        for (var i = 0, length = lineItEls.length; i < length; i++) {
             drawLine(lineItEls[i]);
         }
     }
     return {
-        please : init
+        please: init
     }
 })();
 
 
-window.addEventListener('load',function(e){
+window.addEventListener('load', function (e) {
     lineIt.please();
 });
